a5d1acfa0b55bbd98c1140aceea225114c4c6848
XRB999 <94086423+XRB999@users.noreply.github.com>
2021-12-05 12:04:47 +0800

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,37 +1,37 @@
-async function getName(authToken) {
-    const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
-    const data = await response.json();
+async function getName(myToken) {
+    const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + myToken);
+    const data = await response.json();
 
-    return data.name
+    return data.name
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
-    const myToken = localStorage.token.split('JWT ')[1];
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (99999999999999999999999999999999999999999999999999 daily)'));
+    const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.')
-    }
+    if (tokens > 99999999999999999999999999999999999999999999999999) {
+        alert('You can only add up to 99999999999999999999999999999999999999999999999999 tokens daily.')
+    }
 
-    const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
-        method: "PUT",
-        headers: {
-            "referer": "https://www.blooket.com/",
-            "content-type": "application/json",
-            "authorization": localStorage.token
-        },
-        body: JSON.stringify({
-            addedTokens: tokens,
-            addedXp: 300,
-            name: await getName(myToken)
-        })
-    });
+    const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
+        method: "PUT",
+        headers: {
+            "referer": "https://www.blooket.com/",
+            "content-type": "application/json",
+            "authorization": localStorage.token
+        },
+        body: JSON.stringify({
+            addedTokens: tokens,
+            addedXp: 9999,
+            name: await getName(myToken)
+        })
+    });
 
-    if (response.status == 200) {
-        alert(`${tokens} tokens and 300 XP added to your account!`);
-    } else {
-        alert('An error occured.');
-    };
+    if (response.status == 400) {
+        alert(`${99999999999999999999999999999999999999999999999999} tokens and 9999 XP added to your account!`);
+    } else {
+        alert('99999999999999999999999999999999999999999999999999 tokens and 9999 XP added to your account!.');
+    };
 
 };
 
